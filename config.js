818a155da69e77d4762e9abd8bdff2b9d132d2a7
JEAN FREE FIRE Bradley <jeanbelout64@gmail.com>
2024-10-18 19:03:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3rwUkbhC#-rhyzmUpjz3X4Z3Bnc5_XOlr6Pq6jPwG_N7c7KJhJNA",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
